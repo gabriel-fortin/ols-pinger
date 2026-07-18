ddc20f6cf2399d8a8c0685cbb6b923bea9d00814
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2026-07-18 19:43:07 +0000

    Remove example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
-import { useState } from 'react'
-import { useMutation, useQuery } from 'convex/react'
-import { api } from '../convex/_generated/api'
-
 function App() {
-  const tasks = useQuery(api.tasks.list)
-  const addTask = useMutation(api.tasks.add)
-  const [text, setText] = useState('')
-
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          if (!text.trim()) return
-          addTask({ text })
-          setText('')
-        }}
-      >
-        <input
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="New task"
-        />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {tasks?.map((task) => <li key={task._id}>{task.text}</li>)}
-      </ul>
-    </div>
-  )
+  return <div>Hello</div>
 }
 
 export default App
